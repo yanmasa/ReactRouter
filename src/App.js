eda29f3240104ab70032873898d8ce40d959aeb6
Masaya Ueda <88300005+yanmasa@users.noreply.github.com>
2022-05-16 17:54:17 +0900

    ReactRouterall

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Link } from "react-router-dom";
+
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+      </div>
+      <Router />
+    </BrowserRouter>
   );
 }
